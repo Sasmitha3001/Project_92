f3774137b07a0d6bd6d80fee64ad270792084591
Sasmitha3001 <65769282+Sasmitha3001@users.noreply.github.com>
2021-03-02 18:00:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
+import WelcomeScreen from './Screens/WelcomeScreen'
+import ScheduledClasses from './Screens/ScheduledClasses'
+
+export default class App extends Component {
+  render(){
+    return (
+      <View>
+        <AppContainer/>
+      </View>
+    )
+  }
+  
+}
+
+const SwitchNavigator=createSwitchNavigator({
+  WelcomeScreen:{
+    screen:WelcomeScreen
+  },
+  Home:{
+    screen:ScheduledClasses
+  }
+})
+
+const AppContainer=createAppContainer(SwitchNavigator)
+
---
+++ Screens/ScheduledClasses.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class ScheduledClasses extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Scheduled Classes</Text>
+      </View>
+    )
+  }
+  
+}
+
